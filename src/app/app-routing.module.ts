1cbbf2ea9c9d11c4839cfc0cd1db53eade247a63
NewSteven <36870147+DeibyPeralta@users.noreply.github.com>
2022-04-26 21:04:23 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HackatonComponent } from './vista/hackaton/hackaton/hackaton.component';
+import { IndexComponent } from './vista/index/index/index.component';
+import { InscripcionesComponent } from './vista/inscripciones/inscripciones/inscripciones.component';
+import { PreguntasComponent } from './vista/preguntas/preguntas/preguntas.component';
+import { QuienesSomosComponent } from './vista/QuienesSomos/quienes-somos/quienes-somos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HackatonComponent},
+  {path: 'quienes-somos', component: QuienesSomosComponent},
+  {path: 'hackaton', component: HackatonComponent},
+  {path: 'inscripciones', component: InscripcionesComponent},
+  {path: 'preguntas-frecuentes', component: PreguntasComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './vista/index/index/index.component';
+import { QuienesSomosComponent } from './vista/QuienesSomos/quienes-somos/quienes-somos.component';
+import { HackatonComponent } from './vista/hackaton/hackaton/hackaton.component';
+import { InscripcionesComponent } from './vista/inscripciones/inscripciones/inscripciones.component';
+import { PreguntasComponent } from './vista/preguntas/preguntas/preguntas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    QuienesSomosComponent,
+    HackatonComponent,
+    InscripcionesComponent,
+    PreguntasComponent,
   ],
   imports: [
     BrowserModule,
